068af234150fbcd5d096dc25fa50488fd9d70a7c
Nevena Tripalo <ntripalo@pmfst.hr>
2026-03-14 16:33:53 +0100

    Zadatak 3: pravac završno

--- Vjezbe/Vjezbe1/zadatak3.py
+++ Vjezbe/Vjezbe1/zadatak3.py
@@ -11,8 +11,8 @@ for i in range (2):
         x=input(f"Unesite x koordinatu za {i+1}. točku: ")
         y=input(f"Unesite y koordinatu za {i+1}. točku: ")
         try:
-            x=int(x)
-            y=int(y)
+            x=float(x)
+            y=float(y)
         except:
             print("Pokušajte ponovno unijeti koordinatu")
         else:
@@ -24,11 +24,26 @@ x1=koordinate_x[0]
 y1=koordinate_y[0]
 x2=koordinate_x[1]
 y2=koordinate_y[1]
+
+#provjera jesu li točke iste: beskonačno mnogo pravaca
+if x1 == x2 and y1 == y2:
+    print("Unijeli ste dvije iste točke. Kroz jednu točku prolazi beskonačno mnogo pravaca.")
+    exit()
+
 print(f"Koordinate prve točke su {x1, y1}")
 print(f"Koordinate druge točke su {x2, y2}")
-
+if x1==x2:
+   print(f"Jednadžba ovoga pravca glasi x= {x1}") 
+elif y1==y2:
+   print(f"Jednadžba ovoga pravca glasi y= {y1}") 
 #rješavanje dvije jednadžbe s dvije nepoznanice (y1=ax1+b, y2=ax2+b)
-a=(y1-y2)/(x1-x2)
-b=y1-a*x1
-print(f"Jednadžba ovoga pravca glasi y={a}x+{b}")
+else:
+    a=(y1-y2)/(x1-x2)
+    a=round(a,2)
+    b=y1-a*x1
+    b=round(b,2)
+    if b<0:
+        print(f"Jednadžba ovoga pravca glasi y={a}x {b}")
+    else:
+        print(f"Jednadžba ovoga pravca glasi y={a}x+{b}")
         
