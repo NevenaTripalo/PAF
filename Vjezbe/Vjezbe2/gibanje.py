a6353e221819e54ecd0bf22f429d78def697c479
Nevena Tripalo <ntripalo@pmfst.hr>
2026-04-06 20:36:29 +0200

    provjera i pozivanje modula particle.py

---
+++ Vjezbe/Vjezbe2/gibanje.py
@@ -0,0 +1,15 @@
+#Koristeći klasu Particle u programu "gibanje.py" kreirajte jednan objekt i postavite ga na neke od vrijednosti
+#za koje ste analitički izračunali domet. Da li se numeričko riješenje slaže s analitičkim? Koliko je odstupanje?
+from particle import Particle
+import math
+dt=0.001
+cestica1=Particle(v0=40, kut=45, x0=0, y0=0)
+cestica1.range(dt)
+# analitičko rješenje
+v0 = 40
+theta = math.radians(45)
+g = 9.81
+analitički = v0**2 * math.sin(2*theta) / g
+
+print("Analitički domet:", analitički)
+print("Relativna pogreška:", abs(cestica1.range(dt) - analitički) / analitički)
