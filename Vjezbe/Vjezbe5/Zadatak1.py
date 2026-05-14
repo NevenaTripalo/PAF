230913014f7c38084f32f810f6436153b43b5d16
Nevena Tripalo <ntripalo@pmfst.hr>
2026-05-14 21:53:23 +0200

    Zadatak 1: zbrajanje i oduzimanje

---
+++ Vjezbe/Vjezbe5/Zadatak1.py
@@ -0,0 +1,11 @@
+#(a) Oduzmite 5.0 i 4.935. Koji rezultat očekujete?
+#Koji rezultat dobijete koristeći Python? Objasnite.
+#(b) Provjerite iznosi li suma brojeva 0.1, 0.2 i 0.3 broj 0.6.
+#Objasnite rezultat koji ste dobili.
+
+print(5.0-4.935)
+#rezultat koji ocekujemo je 0.065
+#0.06500000000000039 rezultat koji se dobije s pythonom,
+# do toga dolazi zbog nacina na koji python pretvara decimalne brojeve u binarni sustav
+print(0.1+0.2+0.3)
+#0.6000000000000001
