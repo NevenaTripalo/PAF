1f3021e60948a9c4b7122fc1504f4227e1917b65
Nevena Tripalo <ntripalo@pmfst.hr>
2026-06-03 17:02:28 +0200

    zadatak3

--- Vjezbe/Vjezbe5/arithm.py
+++ Vjezbe/Vjezbe5/arithm.py
@@ -9,8 +9,7 @@ tocke = [1, 2, 4, 6, 10, 16, 3, 5, 11, 15]
 srednja_vrijednost = stats.mean(tocke)
 
 #standardna devijacija po zadanoj formuli (koristimo mean iz modula)
-suma = sum((x - srednja_vrijednost)**2 for x in tocke)
-sigma = math.sqrt(suma / (n * (n - 1)))
+sigma=stats.stdev(tocke)
 
 # Ispis rezultata
 print(f"Aritmetička sredina s modulima: {srednja_vrijednost}")
